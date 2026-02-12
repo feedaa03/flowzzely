2bbb6fd5419d157da31a7726f017f553b47364e3
Feda <feeeda2003@gmsil.com>
2026-02-12 20:26:21 +0300

    Initial Commit

---
+++ Flowzzley/FlowzzleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowzzleyApp.swift
+//  Flowzzley
+//
+//  Created by Feda  on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlowzzleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlowzzleyTests/FlowzzleyTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlowzzleyTests.swift
+//  FlowzzleyTests
+//
+//  Created by Feda  on 12/02/2026.
+//
+
+import Testing
+
+struct FlowzzleyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
